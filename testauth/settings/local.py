ac27b68446e7e9ebdf65824349ed4f242594fb23
Peter Pfeufer <info@ppfeufer.de>
2023-10-19 21:29:01 +0200

    [MISC] pre-commit updated and applied

--- testauth/settings/local.py
+++ testauth/settings/local.py
@@ -32,17 +32,16 @@ INSTALLED_APPS += [
 ]
 
 # Enter credentials to use MySQL/MariaDB. Comment out to use sqlite3
-"""
-DATABASES['default'] = {
-    'ENGINE': 'django.db.backends.mysql',
-    'NAME': 'alliance_auth',
-    'USER': '',
-    'PASSWORD': '',
-    'HOST': '127.0.0.1',
-    'PORT': '3306',
-    'OPTIONS': {'charset': 'utf8mb4'},
-}
-"""
+# DATABASES['default'] = {
+#     'ENGINE': 'django.db.backends.mysql',
+#     'NAME': 'alliance_auth',
+#     'USER': '',
+#     'PASSWORD': '',
+#     'HOST': '127.0.0.1',
+#     'PORT': '3306',
+#     'OPTIONS': {'charset': 'utf8mb4'},
+# }
+
 
 # Register an application at https://developers.eveonline.com for Authentication
 # & API Access and fill out these settings. Be sure to set the callback URL
